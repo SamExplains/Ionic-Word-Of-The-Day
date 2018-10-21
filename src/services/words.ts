e0afd8bcc4c5bd952fb8fd381f4a26b5f0b9100e
Sampuccino <Sampuccino@gmail.com>
2018-10-20 23:22:57 -0700

    Created a service to help retrieve the favorite words to display on the faavorites page.

---
+++ src/services/words.ts
@@ -0,0 +1,22 @@
+import {Word} from "../data/word.interface";
+
+export class WordsService{
+  private favoriteWords: Word[] = [];
+
+  addWordToFavorites(word: Word){
+    this.favoriteWords.push(word);
+  }
+
+  removeWordFromFavorites(word: Word){
+    const pos = this.favoriteWords.findIndex((wordElem: Word) => {
+      return wordElem.id == word.id;
+    });
+    this.favoriteWords.splice(pos, 1);
+  }
+
+  getFavoriteWords(){
+    return this.favoriteWords.slice();
+  }
+
+
+}
