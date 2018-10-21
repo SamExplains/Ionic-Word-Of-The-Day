05279509afd7cca32bec7c5c8044c57a292a8edc
Sampuccino <Sampuccino@gmail.com>
2018-10-20 23:14:53 -0700

    Working with Ionic alerts and buttons either custom or standard.

--- src/pages/words/words.ts
+++ src/pages/words/words.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import { IonicPage, NavParams } from 'ionic-angular';
+import {AlertController, IonicPage, NavParams} from 'ionic-angular';
 import {Word} from "../../data/word.interface";
 
 @IonicPage()
@@ -10,14 +10,34 @@ import {Word} from "../../data/word.interface";
 export class WordsPage implements OnInit{
   wordGroup: { category: string, words: Word[], icon:string };
 
-  constructor(private navParams: NavParams) {
+  constructor(private navParams: NavParams, private alertCtr: AlertController) {
   }
 
   ngOnInit(){
     this.wordGroup = this.navParams.data;
   }
 
-  onAddToFavorite(){
+  onAddToFavorite(selectedQuote: Word){
+    const alert = this.alertCtr.create({
+      title: 'Add Word',
+      subTitle: 'Is that OK?',
+      message: 'Is this the word you would like to favorite?',
+      buttons: [{
+        text: 'Sure',
+        handler: () => {console.log('Clickity.')},
+      },
+      {
+        text: 'Nope',
+        role: 'cancel',
+        handler: () =>{
+          console.log('No clicked');
+        }
+      }
+      ]
+    });
+
+    alert.present();
+
 
   }
 
